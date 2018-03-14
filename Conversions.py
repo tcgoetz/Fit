27616aa6fc2b57c2d0d9c06d9085125684ce471c
Tom Goetz <tom.goetz@virtualcomputer.com>
2018-03-13 20:05:57 -0400

    Refactor conversion functions

--- Conversions.py
+++ Conversions.py
@@ -10,40 +10,39 @@ import time, datetime
 def ms_to_dt_time(time_ms):
     if time_ms is not None:
         return (datetime.datetime.min + datetime.timedelta(0, 0, 0, time_ms)).time()
-    return None
 
 def secs_to_dt_time(time_secs):
     if time_secs is not None:
         return (datetime.datetime.min + datetime.timedelta(0, time_secs)).time()
-    return None
 
 def min_to_dt_time(time_mins):
     if time_mins is not None:
         return secs_to_dt_time(time_mins * 60)
-    return None
 
 def time_to_timedelta(time):
-    if time is None:
-        return None
-    return datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second)
+    if time is not None:
+        return datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second)
+
+def timedelta_to_time(timedelta):
+    return (datetime.datetime.min + timedelta).time()
 
 def add_time(time1, time2, scale=1):
     return (datetime.datetime.min + time_to_timedelta(time1) + (time_to_timedelta(time2) * scale)).time()
 
+def subtract_time(time1, time2):
+    return (datetime.datetime.min + time_to_timedelta(time1) - time_to_timedelta(time2)).time()
+
 def meters_to_feet(meters):
     if meters is not None:
         return (meters * 3.2808399)
-    return None
 
 def day_of_the_year_to_datetime(year, day):
     return datetime.datetime(year, 1, 1) + datetime.timedelta(day - 1)
 
 def centimeters_to_meters(centimeters):
-    if centimeters is None:
-        return None
-    return (centimeters * 100.0)
+    if centimeters is not None:
+        return (centimeters * 100.0)
 
 def meters_to_miles(meters):
     if meters is not None:
         return meters_to_feet(meters) / 5280.0
-    return None
