ccf5e7ce1bde9c894d1766b874cf1d272c17b226
Tom Goetz <tcgoetz@gmail.com>
2019-11-24 20:05:34 -0500

    add more ins and out for measurement classes

--- measurement.py
+++ measurement.py
@@ -37,6 +37,7 @@ class Distance(Measurement):
     """An object that handles convertions of distance mesaurements from one representation to another."""
 
     def __init__(self, meters, raw_value, invalid_value):
+        """Return an instance of ther Distance class."""
         super(Distance, self).__init__(raw_value, invalid_value, self.to_meters, 'meters')
         self.meters = meters
 
@@ -48,35 +49,52 @@ class Distance(Measurement):
     @classmethod
     def from_cm(cls, cm, invalid_value=None):
         """Create a Distance object from a distance measurement in centimeters."""
-        return cls(cm / 100.0, cm, invalid_value)
+        if cm is not None:
+            return cls(cm / 100.0, cm, invalid_value)
+        return cls(None, cm, invalid_value)
 
     @classmethod
     def from_mm(cls, mm, invalid_value=None):
         """Create a Distance object from a distance measurement in millimeters."""
-        return cls(mm / 1000.0, mm, invalid_value)
+        if mm is not None:
+            return cls(mm / 1000.0, mm, invalid_value)
+        return cls(None, mm, invalid_value)
 
     @classmethod
     def from_feet(cls, feet, invalid_value=None):
         """Create a Distance object from a distance measurement in feet."""
-        return cls(feet / 3.2808399, feet, invalid_value)
+        if feet is not None:
+            return cls(feet / 3.2808399, feet, invalid_value)
+        return cls(None, feet, invalid_value)
+
+    @classmethod
+    def from_meters_or_feet(cls, distance, measurement_system=fe.DisplayMeasure.metric):
+        """Return a distance object."""
+        return cls.from_meters(distance) if measurement_system is fe.DisplayMeasure.metric else cls.from_feet(distance)
 
     def to_mm(self):
-        return self.meters * 1000.0
+        if self.meters is not None:
+            return self.meters * 1000.0
 
     def to_meters(self):
-        return self.meters
+        if self.meters is not None:
+            return self.meters
 
     def to_kms(self):
-        return (self.meters / 1000.0)
+        if self.meters is not None:
+            return (self.meters / 1000.0)
 
     def to_inches(self):
-        return (self.meters * 39.37008)
+        if self.meters is not None:
+            return (self.meters * 39.37008)
 
     def to_feet(self):
-        return (self.meters * 3.2808399)
+        if self.meters is not None:
+            return (self.meters * 3.2808399)
 
     def to_miles(self):
-        return (self.meters * 0.0006213712)
+        if self.meters is not None:
+            return (self.meters * 0.0006213712)
 
     @classmethod
     def mm_or_inches(cls, distance, measurement_system=fe.DisplayMeasure.metric):
@@ -138,26 +156,57 @@ class Speed(Measurement):
     """Object that represents a speed measurement."""
 
     def __init__(self, meters_per_sec, raw_value, invalid_value):
+        """Return a Speed instance intialized with a value in meters per sec."""
         super(Speed, self).__init__(raw_value, invalid_value, self.to_mph, 'mph')
         self.meters_per_sec = meters_per_sec
 
     @classmethod
     def from_mps(cls, meters_per_sec, invalid_value=None):
+        """Return a Speed instance intialized with a value in meters per sec."""
         return cls(meters_per_sec, meters_per_sec, invalid_value)
 
     @classmethod
+    def from_kph(cls, km_per_hour, invalid_value=None):
+        """Return a Speed instance intialized with a value in kilometers per hour."""
+        if km_per_hour is not None:
+            return cls(km_per_hour * 1000.0, km_per_hour, invalid_value)
+        return cls(None, km_per_hour, invalid_value)
+
+    @classmethod
+    def from_mph(cls, miles_per_hour, invalid_value=None):
+        """Return a Speed instance intialized with a value in miles per hour."""
+        if miles_per_hour is not None:
+            return cls(miles_per_hour * 0.44704, miles_per_hour, invalid_value)
+        return cls(None, miles_per_hour, invalid_value)
+
+    @classmethod
     def from_cps(cls, centimeters_per_sec, invalid_value=None):
-        return cls(centimeters_per_sec / 100.0, centimeters_per_sec, invalid_value)
+        """Return a Speed instance intialized with a value in centimeters per sec."""
+        if centimeters_per_sec is not None:
+            return cls(centimeters_per_sec / 100.0, centimeters_per_sec, invalid_value)
+        return cls(None, centimeters_per_sec, invalid_value)
 
     @classmethod
     def from_mmps(cls, mm_per_sec, invalid_value=None):
-        return cls(mm_per_sec / 1000.0, mm_per_sec, invalid_value)
+        """Return a Speed instance intialized with a value in millimeters per sec."""
+        if mm_per_sec is not None:
+            return cls(mm_per_sec / 1000.0, mm_per_sec, invalid_value)
+        return cls(None, mm_per_sec, invalid_value)
+
+    @classmethod
+    def from_kph_or_mph(cls, speed, measurement_system=fe.DisplayMeasure.metric):
+        return cls.from_kph(speed) if measurement_system is fe.DisplayMeasure.metric else cls.from_mph(speed)
 
     def to_kph(self):
-        return ((self.meters_per_sec * (60 * 60)) / 1000.0)
+        if self.meters_per_sec is not None:
+            return ((self.meters_per_sec * (60 * 60)) / 1000.0)
 
     def to_mph(self):
-        return (self.meters_per_sec * 2.236936)
+        if self.meters_per_sec is not None:
+            return (self.meters_per_sec * 2.236936)
+
+    def to_mps(self):
+        return self.meters_per_sec
 
     @classmethod
     def kph_or_mph(cls, speed, measurement_system=fe.DisplayMeasure.metric):
