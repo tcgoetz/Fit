854e275cf36a3a25f588d08bd6dd05985b9a38d5
Tom Goetz <tomcgoetz@icloud.com>
2022-01-09 21:05:48 -0500

    add units for altitude and temp

--- fitfile/units.py
+++ fitfile/units.py
@@ -15,17 +15,23 @@ class UnitTypes(enum.Enum):
     distance_long   = 1
     pace            = 2
     speed           = 3
+    altitude        = 4
+    tempurature     = 5
 
 
 unit_strings = {
     DisplayMeasure.statute : {
         UnitTypes.distance_long : 'miles',
         UnitTypes.pace          : 'per mile',
-        UnitTypes.speed         : 'mph'
+        UnitTypes.speed         : 'mph',
+        UnitTypes.altitude      : 'feet',
+        UnitTypes.tempurature   : 'F'
     },
     DisplayMeasure.metric : {
         UnitTypes.distance_long : 'kilometers',
         UnitTypes.pace          : 'per kilometers',
-        UnitTypes.speed         : 'kph'
+        UnitTypes.speed         : 'kph',
+        UnitTypes.altitude      : 'meters',
+        UnitTypes.tempurature   : 'C'
     }
 }
